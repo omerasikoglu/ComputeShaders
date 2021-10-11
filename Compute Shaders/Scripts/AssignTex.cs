729a591f653ff9b1a4c6bd4cd4c876281381fcdc
omerasikoglu <59120165+omerasikoglu@users.noreply.github.com>
2021-10-12 02:54:56 +0300

    Day-3
    Buffer and CPU-GPU interaction

--- Compute Shaders/Scripts/AssignTex.cs
+++ Compute Shaders/Scripts/AssignTex.cs
@@ -9,6 +9,7 @@ public class AssignTex : MonoBehaviour
 
     Renderer rend;
     RenderTexture outputTex;
+
     int kernelHandle;
 
     private void Start()
@@ -27,6 +28,12 @@ public class AssignTex : MonoBehaviour
         shader.SetTexture(kernelHandle, "Result", outputTex);
         rend.material.SetTexture("_MainTex", outputTex);
         DispatchShader(texResolution / 16, texResolution / 16);
+        /* 
+         * Her texRes 8 thread'den oluşur.
+         * TextRes/8= 1 thread'i verir. Bu bizim çözünürlüğümüzdür.
+         * /16 yaparsak ekranın yarısına gelmiş oluruz x ve y ekseninde yani 
+         * ekranın çeyreği tex ile kaplanmış olur
+         */
     }
 
     private void DispatchShader(int x, int y)
